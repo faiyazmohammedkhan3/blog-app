cd6db03ad0336fc56ee5f807e7a5e4822397d789
faiyazmohammedkhan3 <147011610+faiyazmohammedkhan3@users.noreply.github.com>
2024-05-23 17:28:24 -0500

    WebAPI src

---
+++ WebApi/BlogAPI/Data/BlogRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using BlogAPI.Models;
+
+namespace BlogAPI.Data
+{
+    public class BlogRepository
+    {
+        private readonly string _connectionString;
+
+        public BlogRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IEnumerable<Blog> GetAllBlogs()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Blog>("SELECT * FROM Blogs");
+            }
+        }
+
+        public void AddBlog(Blog blog)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "INSERT INTO Blogs (Title, Content) VALUES (@Title, @Content)";
+                connection.Execute(sql, blog);
+            }
+        }
+    }
+}
---
+++ WebApi/BlogAPI/Startup.cs
@@ -0,0 +1,50 @@
+﻿using BlogAPI.Data;
+using Microsoft.OpenApi.Models;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddScoped<BlogRepository>();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blog API", Version = "v1" });
+        });
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog API v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseCors("AllowAll");
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
